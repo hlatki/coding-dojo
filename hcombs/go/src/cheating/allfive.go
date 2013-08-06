883cf982e12b21901020c20aa7e81549af5fb984
Harold Combs <hcombs@lexmark.com>
2013-08-06 13:58:22 -0400

    Made parallel

--- hcombs/go/src/cheating/allfive.go
+++ hcombs/go/src/cheating/allfive.go
@@ -9,20 +9,24 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"flag"
+	"runtime/pprof"
+	"os"
+	"log"
 )
 
+type HandFrequency [NUM_HAND_TYPES]int
 
-func main() {
+// Check a subslice of hands, starting with 'intiial' and incrementing by
+// step, trying each combination below that.
+func checkHands( deck_ *Deck, initial, step int, out chan HandFrequency ) {
 
-	var deck Deck
 	var hand Hand
-	var freq [NUM_HAND_TYPES]int
+	deck := *deck_
+	var frequency HandFrequency
 
-	var i,j int
-
-	init_deck(&deck)
-
-	for a:=0; a < (CARDS_IN_DECK - 4); a++ {
+	for a:=initial; a < (CARDS_IN_DECK - 4); a+=step {
 		hand[0] = deck[a]
 		for b:=a+1; b < (CARDS_IN_DECK - 3); b++ {
 			hand[1] = deck[b]
@@ -32,17 +36,58 @@ func main() {
 					hand[3] = deck[d]
 					for e:=d+1; e < CARDS_IN_DECK; e++ {
 						hand[4] = deck[e]
-
-						i = hand.eval()
-						j = int(hand_rank(i))
-						freq[j]++
+						frequency[ hand_rank( hand.eval() ) ]++
 					}
 				}
 			}
 		}
 	}
 
-	for i = 1 ; i < NUM_HAND_TYPES ; i++ {
+	//send result to the output channel
+	out <- frequency
+}
+
+var parallels = flag.Int("p", 1, "number of parallel processes")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+func main() {
+
+	var deck Deck
+	var freq HandFrequency
+
+	init_deck(&deck)
+
+	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	runtime.GOMAXPROCS(*parallels)
+
+	out := make( chan HandFrequency )
+
+	for i:=0; i < *parallels; i++ {
+		go checkHands( &deck, i, *parallels, out )
+	}
+
+	//read the results
+	var f1 HandFrequency
+	for i:=0; i < *parallels; i++ {
+		f1 = <- out //read the result
+		for i, val := range f1 {
+			freq[i] += val
+		}
+	}
+
+	close(out)
+
+	for i := 1 ; i < NUM_HAND_TYPES ; i++ {
 		fmt.Printf("%15s: %8d\n", handRankStrings[i], freq[i] )
 	}
 
