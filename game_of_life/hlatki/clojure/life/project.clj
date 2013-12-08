08ad17f0f96b7f4b06bb70dca77540d8e60099da
hlatki <hannahatkinson@gmail.com>
2013-12-08 13:45:53 -0500

    Add project file

---
+++ game_of_life/hlatki/clojure/life/project.clj
@@ -0,0 +1,8 @@
+(defproject life "0.1.0-SNAPSHOT"
+  :description "Implementation of Conways Game of Life"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.5"]]
+  :main life.main)
