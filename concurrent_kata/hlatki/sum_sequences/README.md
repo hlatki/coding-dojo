0d47210f3d81b8b3bb62c0fe6883a3cca727bb2f
hlatki <hannahatkinson@gmail.com>
2014-01-23 22:38:08 -0500

    The basics

---
+++ concurrent_kata/hlatki/sum_sequences/README.md
@@ -0,0 +1,17 @@
+# sum_sequences
+
+A Clojure library designed to ... well, that part is up to you.
+
+## TODO
+[ ] Maybe use lists instead of vectors -- change func in helper and in core (and in gen maybe)
+
+## Usage
+
+FIXME
+
+
+## License
+
+Copyright © 2013 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ concurrent_kata/hlatki/sum_sequences/project.clj
@@ -0,0 +1,7 @@
+(defproject sum_sequences "0.1.0-SNAPSHOT"
+  :description "Finds sequences of numbers s.t. the final number is the sum of the rest"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.5"]])
